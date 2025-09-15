74e4452fe332c193af87483e0bfee35af02eb0fe
Hagartinger <ArturDzabiev@gmail.com>
2025-09-15 10:12:17 +0300

    Added cache folder into fd and rg ignore path for fzf-lua

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,11 +1,18 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = function(_, opts)
 		vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", {})
 		vim.keymap.set("n", "<leader>fh", ":FzfLua helptags<CR>", {})
 		vim.keymap.set("n", "<leader>fg", ":FzfLua grep<CR>", {})
 		vim.keymap.set("n", "<leader>fd", ":FzfLua diagnostics_workspace<CR>", {})
 		vim.keymap.set("n", "<leader>fG", ":FzfLua lsp_workspace_symbols<CR>", {})
-    end,
+
+		opts.files = {
+			rg_opts = [[--color=never --hidden --files -g "!.git" -g "!.cache"]],
+			fd_opts = [[--color=never --hidden --type f --type l --exclude .git --exclude .cache]],
+		}
+
+		return opts
+	end,
 }
